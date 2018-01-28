ce82aaecdf92449ea0b7eadcd025ad5f2a49e3b1
Rohit Gehe <rohit.gehe@efficientbazaar.com>
2018-01-28 17:50:45 +0530

    started writing wallet models

--- wallet/models.py
+++ wallet/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+ASSET_TYPES = (
+    ('FOO', 'Footype'),
+    ('BAR', 'Bartype'),
+)
+
+
+class Currency(models.Model):
+    """
+    Description: Model Description
+    """
+    ticker = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    min_divisible_unit = models.DecimalField(decimal_places=20)
+    symbol = models.CharField(max_length=10)
+    asset_type = models.CharField(max_length=3, choices=ASSET_TYPES)
+
+    class Meta:
+        pass
+
+
+class Address(models.Model):
+    """
+    Description: Model Description
+    """
+    public_key = models.CharField(max_length=None)
+
+    class Meta:
+        pass
