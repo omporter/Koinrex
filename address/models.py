4d5e1f85ccf7091813dea277e3d0cc4925bbc62a
Rohit Gehe <rohit.gehe@efficientbazaar.com>
2018-02-07 23:30:33 +0530

    added an abstract base class to handle all types of crypto addresses and their nuances

--- address/models.py
+++ address/models.py
@@ -1,36 +1,52 @@
 from django.db import models
-from wallet.models import *
+from django.contrib.contenttypes.fields import GenericRelation
 
 from bit import Key
+
+from wallet.models import Wallet
 
 # Create your models here.
 
 
-class BitcoinAddress(models.Model):
+class AddressABC(models.Model):
     """
-    Description: Model Description
+    Description: Abstract base class for Cryptocoin Address model
     """
-    s_key = models.CharField(
+    created_at = models.DateTimeField(auto_now_add=True)
+    ticker = models.CharField(max_length=16)
+    name = models.CharField(max_length=64)
+    currency_min_divisible_unit = models.DecimalField(
+        max_digits=16, decimal_places=16)
+    currency_symbol = models.CharField(max_length=16)
+    sec_key = models.CharField(
         max_length=128, unique=True, editable=False)
-    p_key = models.CharField(
+    pub_key = models.CharField(
         max_length=64, unique=True, editable=False)
 
-    currency = models.OneToOneField(Currency, on_delete=models.PROTECT)
-    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        abstract = True
+
+
+class BitcoinAddress(AddressABC):
+    """
+    Description: Bitcoin Address model which stores and generates the pub/sec key pair
+    """
+    batwa = GenericRelation(Wallet)
 
     def __init__(self, *args, **kwargs):
         super(BitcoinAddress, self).__init__(*args, **kwargs)
-        if self.s_key == '' and self.p_key == '':
+        if self.sec_key == '' and self.pub_key == '':
             # if nothing is there, create, convert and set
-            self.s_key = str(Key().to_hex())
-            self.p_key = str(Key().address)
+            self.sec_key = str(Key().to_hex())
+            self.pub_key = str(Key().address)
+            self.currency_min_divisible_unit = 0.00000001
 
     def Key(self):
-        return Key.from_hex(self.p_key)
+        return Key.from_hex(self.sec_key)
 
     def __str__(self):
-        return self.p_key
+        return self.pub_key
 
     class Meta:
-        verbose_name = 'Address'
-        verbose_name_plural = 'Addresses'
+        verbose_name = 'Bitcoin Address'
+        verbose_name_plural = 'Bitcoin Addresses'
