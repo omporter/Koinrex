5f23baa70b3133479bea3df9f49cb35e65b92612
ananth.adhikarla@gmail.com <ananth.adhikarla@gmail.com>
2018-02-09 22:30:40 +0530

    created model functions

--- moneybag/models.py
+++ moneybag/models.py
@@ -1,15 +1,25 @@
+"""
+
+Changes in AddressABC : Convert BTC to USD till the end of class
+
+Changes in CurrencyABC : added a new function get_list_of_coins 
+
+Need to work on transactions now
+
+"""
+
 from django.db import models
 
 from koinrex.users.models import User
 from moneybag.wrappers.litecoin import LTCKey
-from moneybag.wrappers.coinbin import convert
+from moneybag.wrappers.coinbin import convert,convert_usd
 
 from bit import Key
 
 # Import for converting float to decimal in AddressABC
 
 import decimal
-
+import json
 # Create your models here.
 
 
@@ -57,29 +67,83 @@ class AddressABC(models.Model):
                             decimal.Decimal(convert(user_object.currency_ticker, 'BTC')))
         return btc_val
 
+    # get current usd value based on balance
+    # So from all the coins subclass we check for the subclass of BTC if it exists
+    # if the current BTC balance of user = 0 then return 0
+    # else convert the current balance to USD using the coinbin wrapper and return that value
     @classmethod
-    def get_usd_value():
+    def get_usd_value(cls , user_id):
+        for coin_addr in cls.__subclasses__():
+            user_object = coin_addr.objects.get(user=user_id)
+            if user_object.currency_ticker == 'BTC':
+                if user_object.balance == 0:
+                    return 0
+            else:
+                current_usd_value = decimal.Decimal(convert_usd(user_object.currency_ticker, user_object.balance))
+        return current_usd_value
+
         pass
 
+
+    # *** Below methods needs to be constantly synced with server *** 
+    # *** Both the return statements produce the same results ***
+    # *** Not tested if it makes a difference due to constant update yet ***
+    # *** Not tested example : AddressABC.__transaction_count = something and AddressABC.transaction_count = something ***
+    # *** AddressABC.__transaction_count is accessing transaction_count as a private variable ***
+    # *** Not sure which value to return ***
+    # *** But its mostly wrong I assume ***
     @classmethod
-    def fetch_tx_count():
+    def fetch_tx_count(cls,user_id):
         # blockchain: keep tx count synced
-        pass
+        for coin_addr in cls.__subclasses__():
+            user_object = coin_addr.objects.get(user=user_id)
+            current_tx_count += user_object.transaction_count
+
+        #user_object.transaction_count = current_tx_count    
+        user_object.__transaction_count += current_tx_count
+
+        #return current_tx_count
+        return user_object.__transaction_count      
 
     @classmethod
-    def fetch_balance():
+    def fetch_balance(cls,user_id):
         # blockchain: keep balance synced
-        pass
+        for coin_addr in cls.__subclasses__():
+            user_object = coin_addr.objects.get(user=user_id)
+            current_balance += user_object.balance
+
+        #user_object.balance = current_balance
+        user_object.__balance = current_balance
+
+        #return current_balance
+        return user_object.__balance
+        
 
     @classmethod
-    def fetch_amt_in():
+    def fetch_amt_in(cls,user_id):
         # blockchain
-        pass
+        for coin_addr in cls.__subclasses__():
+            user_object = coin_addr.objects.get(user=user_id)
+            current_ammount_in += user_object.amount_in
+
+        #user_object.amount_in = current_ammount_in
+        user_object.__amount_in = current_ammount_in
+
+        #return current_ammount_in
+        return user_object.__amount_in
 
     @classmethod
-    def fetch_amt_out():
+    def fetch_amt_out(cls,user_id):
         # blockchain
-        pass
+        for coin_addr in cls.__subclasses__():
+            user_object = coin_addr.objects.get(user=user_id)
+            current_ammount_out += user_object.amount_out
+
+        #user_object.amount_out = current_ammount_out
+        user_object.__amount_out = current_ammount_out
+
+        #return current_ammount_out
+        return user_object.__amount_out
 
     class Meta:
         abstract = True
@@ -97,6 +161,24 @@ class CurrencyABC(models.Model):
     currency_min_div_unit = models.DecimalField(
         max_digits=12, decimal_places=12, editable=False)
     currency_symbol = models.CharField(max_length=2, null=True, editable=False)
+    
+    @classmethod
+    def get_list_of_coins(cls,user_id):
+        coin_name = list()
+        c_name = dict()
+        i = 0
+        for coin_list in cls.__subclasses__():
+            coin_name.append(coin_list.objects.get(user=user_id).currency_name)
+            inv = {coin_name[i] : coin_name[i]}
+            c_name.update(inv)
+            i += 1
+
+        # coin_name displays a list json and c_name displays a dictonary json and also the json can be dumped to a temp file
+        # if needed 
+
+        #coin_json = json.dumps(coin_name)
+        coin_json = json.dumps(c_name)
+        return coin_json
 
     class Meta:
         abstract = True
