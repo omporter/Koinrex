45178c31a91596375c18695743609f1041a5a5ab
Rohit Gehe <rohit.gehe@efficientbazaar.com>
2018-02-10 13:51:01 +0530

    cleaned up models and refactored blockchain functions

--- moneybag/models.py
+++ moneybag/models.py
@@ -2,7 +2,7 @@
 
 Changes in AddressABC : Convert BTC to USD till the end of class
 
-Changes in CurrencyABC : added a new function get_list_of_coins 
+Changes in CurrencyABC : added a new function get_list_of_coins
 
 Need to work on transactions now
 
@@ -12,7 +12,11 @@ from django.db import models
 
 from koinrex.users.models import User
 from moneybag.wrappers.litecoin import LTCKey
-from moneybag.wrappers.coinbin import convert,convert_usd
+from moneybag.wrappers.coinbin import convert, convert_usd
+from moneybag.wrappers.blockcypher import address_current_transactions as act
+from moneybag.wrappers.blockcypher import address_current_balance as acb
+from moneybag.wrappers.blockcypher import address_received as a_rec
+from moneybag.wrappers.blockcypher import address_sent as a_sent
 
 from bit import Key
 
@@ -20,6 +24,8 @@ from bit import Key
 
 import decimal
 import json
+
+
 # Create your models here.
 
 
@@ -35,25 +41,28 @@ class AddressABC(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     transaction_count = models.IntegerField()
     balance = models.DecimalField(max_digits=32, decimal_places=8)
-    amount_in = models.DecimalField(max_digits=32, decimal_places=8)
-    amount_out = models.DecimalField(max_digits=32, decimal_places=8)
+    amount_received = models.DecimalField(max_digits=32, decimal_places=8)
+    amount_sent = models.DecimalField(max_digits=32, decimal_places=8)
     # TODO add trading balance
 
-    @classmethod  # TODO modify to return a dictionary of values against its corresponding coin
-    def get_total_balance(cls, user_id):
-        bal = 0
+    # Returns JSON for user's coins and their corresponding balances.
+    @classmethod
+    def get_all_balance(cls, user_id):
+        total = {}
         for coin_addr in cls.__subclasses__():
-            bal += coin_addr.objects.get(user=user_id).balance
-        return bal
+            user_object = coin_addr.objects.get(user=user_id)
+            total[user_object.currency_name] = user_object.balance
+        return total
 
-    @classmethod  # Returns each instance of user's wallet (different coins)
+    # Returns a list of address instances of a user (different coins)
+    @classmethod
     def get_user_moneybag(cls, user_id):
         moneybag = []
         for coin_addr in cls.__subclasses__():
             moneybag.append(coin_addr.objects.get(user=user_id))
         return moneybag
 
-    # 8 decimal places
+    # TODO 8 decimal places
     @classmethod
     def get_btc_value(cls, user_id):
         btc_val = 0
@@ -67,83 +76,83 @@ class AddressABC(models.Model):
                             decimal.Decimal(convert(user_object.currency_ticker, 'BTC')))
         return btc_val
 
-    # get current usd value based on balance
-    # So from all the coins subclass we check for the subclass of BTC if it exists
-    # if the current BTC balance of user = 0 then return 0
-    # else convert the current balance to USD using the coinbin wrapper and return that value
+    # Returns a user's total USD balance converted with Coinbin API
     @classmethod
-    def get_usd_value(cls , user_id):
+    def get_usd_value(cls, user_id):
+        current_usd_value = 0
         for coin_addr in cls.__subclasses__():
             user_object = coin_addr.objects.get(user=user_id)
-            if user_object.currency_ticker == 'BTC':
-                if user_object.balance == 0:
-                    return 0
-            else:
-                current_usd_value = decimal.Decimal(convert_usd(user_object.currency_ticker, user_object.balance))
+            current_usd_value += decimal.Decimal(convert_usd(
+                user_object.currency_ticker, user_object.balance))
         return current_usd_value
 
-        pass
+    """
+    Methods below are to be wrapped in a queue
+    """
 
-
-    # *** Below methods needs to be constantly synced with server *** 
-    # *** Both the return statements produce the same results ***
-    # *** Not tested if it makes a difference due to constant update yet ***
-    # *** Not tested example : AddressABC.__transaction_count = something and AddressABC.transaction_count = something ***
-    # *** AddressABC.__transaction_count is accessing transaction_count as a private variable ***
-    # *** Not sure which value to return ***
-    # *** But its mostly wrong I assume ***
+    # Fetches all current transactions from blockchain for all addresses of a
+    # given user
     @classmethod
-    def fetch_tx_count(cls,user_id):
-        # blockchain: keep tx count synced
+    def fetch_tx_count(cls, user_id):
+        updated = dict()
         for coin_addr in cls.__subclasses__():
             user_object = coin_addr.objects.get(user=user_id)
-            current_tx_count += user_object.transaction_count
+            address = str(user_object.pub_key)
+            ticker = str(user_object.currency_ticker).lower()
+            current_tx_count = act(address, ticker)
+            user_object.transaction_count = current_tx_count
+            user_object.save()
+            updated.update(
+                {user_object: [ticker, address, user_object.transaction_count]})
+        return updated
 
-        #user_object.transaction_count = current_tx_count    
-        user_object.__transaction_count += current_tx_count
-
-        #return current_tx_count
-        return user_object.__transaction_count      
-
+    # Fetches all current balances from blockchain for all addresses of a
+    # given user
     @classmethod
-    def fetch_balance(cls,user_id):
-        # blockchain: keep balance synced
+    def fetch_balance(cls, user_id):
+        updated = dict()
         for coin_addr in cls.__subclasses__():
             user_object = coin_addr.objects.get(user=user_id)
-            current_balance += user_object.balance
+            address = str(user_object.pub_key)
+            ticker = str(user_object.currency_ticker).lower()
+            current_balance = acb(address, ticker)
+            user_object.balance = current_balance
+            user_object.save()
+            updated.update(
+                {user_object: [ticker, address, user_object.balance]})
+        return updated
 
-        #user_object.balance = current_balance
-        user_object.__balance = current_balance
-
-        #return current_balance
-        return user_object.__balance
-        
-
+    # Fetches all amount received's from blockchain for all addresses of a
+    # given user
     @classmethod
-    def fetch_amt_in(cls,user_id):
-        # blockchain
+    def fetch_amt_received(cls, user_id):
+        updated = dict()
         for coin_addr in cls.__subclasses__():
             user_object = coin_addr.objects.get(user=user_id)
-            current_ammount_in += user_object.amount_in
+            address = str(user_object.pub_key)
+            ticker = str(user_object.currency_ticker).lower()
+            current_received = a_rec(address, ticker)
+            user_object.amount_received = current_received
+            user_object.save()
+            updated.update(
+                {user_object: [ticker, address, user_object.amount_received]})
+        return updated
 
-        #user_object.amount_in = current_ammount_in
-        user_object.__amount_in = current_ammount_in
-
-        #return current_ammount_in
-        return user_object.__amount_in
-
+    # Fetches all amount sent's from blockchain for all addresses of a
+    # given user
     @classmethod
-    def fetch_amt_out(cls,user_id):
-        # blockchain
+    def fetch_amt_sent(cls, user_id):
+        updated = dict()
         for coin_addr in cls.__subclasses__():
             user_object = coin_addr.objects.get(user=user_id)
-            current_ammount_out += user_object.amount_out
-
-        #user_object.amount_out = current_ammount_out
-        user_object.__amount_out = current_ammount_out
-
-        #return current_ammount_out
-        return user_object.__amount_out
+            address = str(user_object.pub_key)
+            ticker = str(user_object.currency_ticker).lower()
+            current_sent = a_sent(address, ticker)
+            user_object.amount_sent = current_sent
+            user_object.save()
+            updated.update(
+                {user_object: [ticker, address, user_object.amount_sent]})
+        return updated
 
     class Meta:
         abstract = True
@@ -161,22 +170,22 @@ class CurrencyABC(models.Model):
     currency_min_div_unit = models.DecimalField(
         max_digits=12, decimal_places=12, editable=False)
     currency_symbol = models.CharField(max_length=2, null=True, editable=False)
-    
+
     @classmethod
-    def get_list_of_coins(cls,user_id):
+    def get_list_of_coins(cls, user_id):
         coin_name = list()
         c_name = dict()
         i = 0
         for coin_list in cls.__subclasses__():
             coin_name.append(coin_list.objects.get(user=user_id).currency_name)
-            inv = {coin_name[i] : coin_name[i]}
+            inv = {coin_name[i]: coin_name[i]}
             c_name.update(inv)
             i += 1
 
-        # coin_name displays a list json and c_name displays a dictonary json and also the json can be dumped to a temp file
-        # if needed 
+        # coin_name displays a list json and c_name displays a dictonary json
+        # and also the json can be dumped to a temp file if needed
 
-        #coin_json = json.dumps(coin_name)
+        # coin_json = json.dumps(coin_name)
         coin_json = json.dumps(c_name)
         return coin_json
 
