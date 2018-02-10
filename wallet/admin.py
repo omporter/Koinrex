2bde7ef4edc3a401d743d5b7a56ecd265aa50cdb
Rohit Gehe <rohit.gehe@efficientbazaar.com>
2018-02-10 14:17:04 +0530

    removed wallet and address apps from the repo, for some reason it did not get cleaned up during merge :expressionless:

--- wallet/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Wallet, Transaction
-# Register your models here.
-
-# admin.site.register(Currency)
-admin.site.register(Wallet)
-admin.site.register(Transaction)
