3071b722ca93d3eaaf1d3fb5524b274ee8d24d51
sanojdon <sanoj.mv.93@gmail.com>
2018-02-11 22:04:54 +0530

    added the total btc and udc_value dynamically

--- moneybag/views.py
+++ moneybag/views.py
@@ -13,11 +13,19 @@ def home(request):
     #Get current user id
     current_user = request.user
     current_user_id = current_user.id
-    
+    #Get all the values needed for the table
     results = AddressABC.get_all_balance(current_user_id).content.decode()
     result = ast.literal_eval(results)
-     
-    context = {'coins': result}
+
+    total_btc_value = AddressABC.get_total_btc_value(current_user_id)
+    total_btc_value = round(total_btc_value, 8)
+        
+    usd_value = AddressABC.get_usd_value(current_user_id)
+    usd_value = round(usd_value, 2)
+         
+    context = { 'coins': result,
+                'usd_value' : usd_value,
+                'total_btc' : total_btc_value}
     return render(request, 'moneybag/wallet_home.html', context)
 
 def withdrawals(request):
